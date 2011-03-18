250bad144e5abf8f25200311733912b4bb4da096
cbrown <cbrown@decisionpatterns.com>
2011-03-17 19:29:41 -0700

    Updating Contact Information

--- R/zzz.R
+++ R/zzz.R
@@ -1,33 +1,14 @@
 
-.onLoad <- function( libname, pkgname ) {
+# .First.lib <- function( libname, pkgname ) {
+.onAttach <- function( libname, pkgname ) {
 
-#  if( is.null( getOption( "odg.logo" ) ) && ! getOption("verbose") ) {
-#    width <- getOption("width")
-#    options( width=80 )
-#
-#    cat(
-#      "                            _       _        ",
-#      "  ___  _ __   ___ _ __   __| | __ _| |_ __ _ ",
-#      " / _ \\| '_ \\ / _ \\ '_ \\ / _` |/ _' | __/ _' |",
-#      "| (_) | |_) |  __/ | | | (_| | (_| | || (_| |",
-#      " \\___/| .__/ \\___|_| |_|\\__,_|\\__,_|\\__\\__,_|",
-#      "      |_|       http://www.opendatagroup.com\n",
-#      sep="\n"
-#    )
-#    options( width=width )
-#    options( odg.logo = TRUE )
-#
-#  } 
-  
-  cat( 
-    "\n  ", 
-    pkgname , 
-    "-" , 
-    utils::installed.packages()[ pkgname , "Version"], 
-    " provided by Open Data.\n" ,
-    sep = ""
+  packageStartupMessage( 
+    pkgname ,
+    "-" ,
+    utils::installed.packages()[ pkgname , "Version"],
+    " provided by Decision Patterns\n" ,
+    domain = NA
   )
 
 }
-
 
