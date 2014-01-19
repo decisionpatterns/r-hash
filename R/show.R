f1c484ad07acaa8380e851441fc909b57487a8e2
Christopher Brown <cbrown@decisionpatterns.com>
2014-01-19 09:50:44 -0800

    Fix formatting in show.R

--- R/show.R
+++ R/show.R
@@ -7,9 +7,34 @@
 #
 #  See Also: print
 # -----------------------------------------------------------------------------
+
 setMethod( "show" , "hash" ,
-	function(object) cat(format(object))
+  function( object )  { 
+    message( "<hash> containing ", length(object), " key-value pair(s)." )
+    for( k in keys(object) ) {
+      
+      message( "  ", k, " : ", appendLF=FALSE )
+      
+      vals <- object[[k]]
+      len <- length(vals)
+      max.print <- getOption("max.print")
+      too_large <- len > max.print
+      
+      if( too_large ) vals <- vals[ 1:max.print ]  
+      cat( paste( as.character( object[[k]] ), collapse=", " ) )
+      
+      if( too_large ) 
+        message( "  Reached max.print (", max.print, ") -- omitted ",
+          len - max.print, " entries.\n", appendLF=FALSE   )
+      
+      cat("\n" )
+    }
+  }
 )
+
+# setMethod( "show" , "hash" ,
+#	function(object) cat(format(object))
+# )
     
     
     
